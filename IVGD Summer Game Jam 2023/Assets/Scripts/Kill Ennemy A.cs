d4267d7ff965f93c944854ac3579a930c5f82202
ET-wont-phone-home <ermorinville@hotmail.fr>
2023-08-06 15:13:05 -0400

    kill ennemy !

--- IVGD Summer Game Jam 2023/Assets/Scripts/Kill Ennemy A.cs
+++ IVGD Summer Game Jam 2023/Assets/Scripts/Kill Ennemy A.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DieFromWA : MonoBehaviour
+public class KillEnnemyA : MonoBehaviour
 {
-    public GameObject ownEnnemy;
-    
+    public GameObject ownSelf;
+
     void OnTriggerEnter(Collider targetObj)
     {
         if (targetObj.gameObject.tag == "WeaponA")
-            Destroy(targetObj.gameObject);
+        Object.Destroy(this.ownSelf);
 
 
     }
